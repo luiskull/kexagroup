bd895051fdb2aed01cf543ee823e06e62b85541f
Luis Bujanda <bujandaluis1@gmail.com>
2026-07-03 10:51:37 -0400

    Update imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,24 @@ function App() {
         <source
           media="(max-width: 768px)"
           srcSet="
-            /coming-soon-mobile.jpg 402w,
-            /coming-soon-mobile@2x.jpg 804w,
-            /coming-soon-mobile@3x.jpg 1206w,
-            /coming-soon-mobile@4x.jpg 1608w
+            /coming-soon-mobile.png 402w,
+            /coming-soon-mobile@2x.png 804w,
+            /coming-soon-mobile@3x.png 1206w,
+            /coming-soon-mobile@4x.png 1608w
           "
           sizes="100vw"
         />
+        <source
+          media="(min-width: 1920px)"
+          srcSet="/coming-soon@4x.png"
+        />
         <img
-          src="/coming-soon.jpg"
+          src="/coming-soon.png"
           srcSet="
-            /coming-soon.jpg 1024w,
-            /coming-soon@2x.jpg 2048w,
-            /coming-soon@3x.jpg 3072w,
-            /coming-soon@4x.jpg 4096w
+            /coming-soon.png 1024w,
+            /coming-soon@2x.png 2048w,
+            /coming-soon@3x.png 3072w,
+            /coming-soon@4x.png 4096w
           "
           sizes="100vw"
           alt="Kexa Group - Coming Soon"
